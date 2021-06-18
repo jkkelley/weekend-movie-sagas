af2240aaa4c3e50f27e910b2082a65de66be62c3
James Kelley <jkkelley86@gmail.com>
2021-06-18 15:44:22 -0500

    added click handler

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,6 +1,14 @@
+import { useHistory } from "react-router-dom";
+
 function MovieItem({ movie }) {
+  // Set useHistory to a variable
+  const history = useHistory();
+  // Handle clicking on the image
+  const handleImageClick = () => {
+    console.log(`handleImageClick pressed`);
+  };
   return (
-    <div>
+    <div onClick={handleImageClick}>
       <h3>{movie.title}</h3>
       <img src={movie.poster} alt={movie.title} />
     </div>
