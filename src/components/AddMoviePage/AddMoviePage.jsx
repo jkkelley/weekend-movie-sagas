93993bc5ee7990d865d29796b39c2b5abcb64ed1
James Kelley <jkkelley86@gmail.com>
2021-06-19 10:54:22 -0500

    Add Movie Title conditional rendering complete.

--- src/components/AddMoviePage/AddMoviePage.jsx
+++ src/components/AddMoviePage/AddMoviePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 
 // Material-ui Imports
@@ -7,13 +7,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     "& > *": {
-        margin: theme.spacing(1),
-      }
+      margin: theme.spacing(1),
+    },
   },
   paper: {
     padding: theme.spacing(2),
@@ -23,6 +24,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function AddMoviePage() {
+  // Area to hold our local states
+  const [flipState, setFlipState] = useState(false);
+
+  const [movieTitleInput, setMovieTitleInput] = useState("");
+  const [movieTitleStatus, setMovieTitleStatus] = useState(false);
   // Bring useHistory in
   const history = useHistory();
   // Custom CSS
@@ -34,14 +40,55 @@ function AddMoviePage() {
     history.push("/");
   };
 
+  // Function to handle Save and POST to Database
+  const handlePostAndSave = () => {
+    console.log(`You clicked handlePostAndSave.`);
+  };
+
+  const handleFlip = (event) => {
+    event.preventDefault();
+    setFlipState(!flipState);
+  };
+
+  const handleFlips = (event) => {
+    event.preventDefault();
+    setFlipState(!flipState);
+    setMovieTitleStatus(!movieTitleStatus);
+
+  };
+
+  const handleMovieTitleStatus = () => {
+    // event.preventDefault();
+    setFlipState(false);
+    setMovieTitleStatus(false);
+  };
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}>Add A Movie</Paper>
+          <Paper className={classes.paper}>Add A New Movie</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>ADD Movie Title</Paper>
+          {!flipState ? (
+            !movieTitleStatus ? (
+              <Paper className={classes.paper} onClick={handleFlip}>
+                Add Movie Title Here
+              </Paper>
+            ) : (
+              <p>Movie Title: {movieTitleInput}</p>
+            )
+          ) : (
+            <div>
+              <TextField
+                id="standard-read-only-input"
+                label="Movie Title"
+                onChange={(event) => setMovieTitleInput(event.target.value)}
+              />
+              <Button color="primary" onClick={handleFlips}>
+                Confirm
+              </Button>
+            </div>
+          )}
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>Movie Poster URL</Paper>
@@ -59,12 +106,14 @@ function AddMoviePage() {
           {/* <Paper className={classes.paper}></Paper> */}
         </Grid>
         <Grid item xs={6} sm={3}>
-            <Button variant="outlined" onClick={handleToHome}>
-              Cancel
-            </Button>
+          <Button variant="outlined" onClick={handleToHome}>
+            Cancel
+          </Button>
         </Grid>
         <Grid item xs={6} sm={3}>
-          <Paper className={classes.paper}>Save Button</Paper>
+          <Button variant="outlined" onClick={handlePostAndSave}>
+            Save
+          </Button>
         </Grid>
       </Grid>
     </div>
