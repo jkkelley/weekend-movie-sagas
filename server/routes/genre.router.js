49550b633cb67f0241884508d57e452d6ad7f823
James Kelley <jkkelley86@gmail.com>
2021-06-19 12:44:15 -0500

    drop down menu works

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require("../modules/pool");
 
 router.get("/:id", (req, res) => {
-  console.log(req.params.id)
+  console.log(req.params.id);
   const queryText = `
     SELECT "genres".name, "movies".title FROM "movies_genres"
     JOIN "genres" on "movies_genres".genre_id = "genres".id
@@ -18,6 +18,24 @@ router.get("/:id", (req, res) => {
     })
     .catch((error) => {
       console.log(`We had a problem processing your error... ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+router.get("/", (req, res) => {
+  // Get our queryText ready
+  const queryText = `
+    SELECT * FROM "genres";
+  `;
+  // Time for a dive...
+  pool
+    .query(queryText)
+    .then((results) => {
+      res.send(results.rows);
+    })
+    .catch((error) => {
+      console.log(`We had a problem processing your Genre List... ${error}`);
+      res.sendStatus(500);
     });
 });
 
