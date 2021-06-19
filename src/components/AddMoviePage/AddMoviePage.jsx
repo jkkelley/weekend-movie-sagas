6635f7c1125180b7b16366926b43915cb154f434
James Kelley <jkkelley86@gmail.com>
2021-06-19 09:37:44 -0500

    Cancel button takes you home.

--- src/components/AddMoviePage/AddMoviePage.jsx
+++ src/components/AddMoviePage/AddMoviePage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useHistory, Link } from "react-router-dom";
+
+// Material-ui Imports
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
+
+import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    "& > *": {
+        margin: theme.spacing(1),
+      }
   },
   paper: {
     padding: theme.spacing(2),
@@ -15,7 +23,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function AddMoviePage() {
+  // Bring useHistory in
+  const history = useHistory();
+  // Custom CSS
   const classes = useStyles();
+
+  // Function to handle route to add movie form
+  const handleToHome = () => {
+    console.log(`You've clicked handleToHome.`);
+    history.push("/");
+  };
 
   return (
     <div className={classes.root}>
@@ -24,7 +41,7 @@ function AddMoviePage() {
           <Paper className={classes.paper}>Add A Movie</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Paper className={classes.paper}>Movie Title</Paper>
+          <Paper className={classes.paper}>ADD Movie Title</Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>Movie Poster URL</Paper>
@@ -34,6 +51,20 @@ function AddMoviePage() {
         </Grid>
         <Grid item xs={12} sm={6}>
           <Paper className={classes.paper}>Drop Menu Genres</Paper>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          {/* <Paper className={classes.paper}></Paper> */}
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          {/* <Paper className={classes.paper}></Paper> */}
+        </Grid>
+        <Grid item xs={6} sm={3}>
+            <Button variant="outlined" onClick={handleToHome}>
+              Cancel
+            </Button>
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <Paper className={classes.paper}>Save Button</Paper>
         </Grid>
       </Grid>
     </div>
