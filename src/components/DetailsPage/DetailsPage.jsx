91dde7b26f45f23522ee469f5450a5f6b3cf4b61
James Kelley <jkkelley86@gmail.com>
2021-06-20 13:59:22 -0500

    genres stays on refresh.

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from "react";
+import { useEffect} from "react";
 import { useHistory, useParams } from "react-router-dom";
 import "./DetailsPage.css";
 // Genres component import
@@ -9,7 +9,7 @@ import Genres from "../Genres/Genres";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import { MovieSharp } from "@material-ui/icons";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,25 +21,17 @@ const useStyles = makeStyles((theme) => ({
 
 function DetailsPage() {
   const params = useParams();
-  // console.log(params);
+
   // Bring useHistory in
   const history = useHistory();
   // Custom CSS
   const classes = useStyles();
   // Bring Dispatch in
   const dispatch = useDispatch();
-  // Bring in our movieItem to GET data from Database
-  const movieItem = useSelector((store) => store.movieItem);
   // Bring in our genres
   const genres = useSelector((store) => store.genres);
-  const movies = useSelector((store) => store.movies);
+  // const movies = useSelector((store) => store.movies);
   const movie = useSelector((store) => store.movie);
-  // console.log(movie)
-  console.log(movies[0]);
-  console.log(params.id);
-  const getGenres = () => {
-    dispatch({ type: "FETCH_GENRES", payload: movieItem.id });
-  };
 
   // Function to handle back to movie list
   const handleBackToMovies = () => {
@@ -52,21 +44,14 @@ function DetailsPage() {
     history.push(`/edit/${movieItem.id}`);
   };
 
-  const [title, setTitle] = useState();
-  const [poster, setPoster] = useState();
-  const [description, setDescription] = useState();
-
-  // const fetchMovieDetails = () => {
-  //   if (params.id == movies[0].id) {
-  //     setTitle(movies[0].title);
-  //     setPoster(movies[0].poster);
-  //     setDescription(movies[0].description);
-  //   }
-  // };
+  // Keep genres on refresh
   useEffect(() => {
-    dispatch({ type: "FETCH_MOVIE_ITEM", payload:  Number(params.id) });
-    // fetchMovieDetails();
-    // getGenres();
+    dispatch({ type: "FETCH_GENRES", payload: Number(params.id) });
+  }, [])
+
+  // Keeps movie item on refresh
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIE_ITEM", payload: Number(params.id) })
   }, []);
 
   return (
@@ -86,7 +71,7 @@ function DetailsPage() {
       <div className="details-page-ul-container">
         <h3>Genres</h3>
         <ul>
-          {genres.map((genres, index) => (
+          {genres?.map((genres, index) => (
             <Genres key={index} genres={genres} />
           ))}
         </ul>
